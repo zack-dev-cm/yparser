663ccc904fab0d6941072f786fd229e3929252ab
zack-dev-cm <112114097+zack-dev-cm@users.noreply.github.com>
2025-06-14 03:22:53 +0400

    Add Colab usage example

---
+++ examples/colab_example.py
@@ -0,0 +1,23 @@
+"""Google Colab example for yparser.
+This script shows how to parse a few image URLs on Colab.
+"""
+
+from yparser.parser import YParser
+
+image_urls = [
+    "https://i.ytimg.com/vi/bj4QiFmFy2M/maxresdefault.jpg",
+    "https://www.ejin.ru/wp-content/uploads/2019/07/3d064e1dc7ce5b4.jpg",
+]
+
+SAVE_PATH = "imgs"
+
+parser = YParser(
+    name="imgs",
+    save_folder=SAVE_PATH,
+    download_workers=2,
+    parser_workers=1,
+    limits=[10, 20],
+    wandb_log=False,
+)
+
+parser.parse(links=image_urls)
